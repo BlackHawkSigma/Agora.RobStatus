c991bb883c3512bf544deefb3446d2edc39b4f92
Tobias Beese <tobias.beese@gmx.de>
2018-01-10 11:55:54 +0100

    Add powershell scripts

---
+++ RestartMicroservice.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+echo restart mircoservice
+pm2 restart robStat
+
+pause
+
+echo done
---
+++ StammdatenUpdate.cmd
@@ -0,0 +1,8 @@
+@ECHO off
+
+echo Erstelle Stammdaten File
+node createMasterData.js
+
+pause
+
+echo Erledigt
