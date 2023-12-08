18871d96394b2a17134d5e8d9a8ac73ebe59cbf0
Tobias Beese <tobias.beese@gmx.de>
2023-12-08 12:35:04 +0100

    Adapt new order in Stammdaten

--- parseMasterData.js
+++ parseMasterData.js
@@ -10,40 +10,45 @@ module.exports = () => {
 
       const rawData = d3.tsvParseRows(data)
 
-      const dataCollection = rawData.map(row => {
+      const dataCollection = rawData.map((row) => {
         return {
-          artikel : row[0],
-          farbe : row[5],
-          farbcode : row[8],
-          typcode : row[10],
-          FL : parseInt(row[4].slice(31, -9), 2),
-          PR : parseInt(row[4].slice(22, -18), 2),
-          BC : parseInt(row[4].slice(9, -27), 2),
-          CC : parseInt(row[4].slice(2, -36), 2)
+          artikel: row[0],
+          farbe: row[6],
+          farbcode: row[9],
+          typcode: row[11],
+          FL: parseInt(row[5].slice(31, -9), 2),
+          PR: parseInt(row[5].slice(22, -18), 2),
+          BC: parseInt(row[5].slice(9, -27), 2),
+          CC: parseInt(row[5].slice(2, -36), 2),
         }
       })
 
-      const nestedData = d3.nest()
-        .key(d => d.typcode)
-        .key(d => d.farbcode)
-        .rollup(v => {
+      const nestedData = d3
+        .nest()
+        .key((d) => d.typcode)
+        .key((d) => d.farbcode)
+        .rollup((v) => {
           return {
-            Artikel : d3.min(v, d => d.artikel),
-            Farbe : d3.min(v, d => d.farbe),
-            FL : d3.max(v, d => d.FL),
-            PR : d3.max(v, d => d.PR),
-            BC : d3.max(v, d => d.BC),
-            CC : d3.max(v, d => d.CC)
+            Artikel: d3.min(v, (d) => d.artikel),
+            Farbe: d3.min(v, (d) => d.farbe),
+            FL: d3.max(v, (d) => d.FL),
+            PR: d3.max(v, (d) => d.PR),
+            BC: d3.max(v, (d) => d.BC),
+            CC: d3.max(v, (d) => d.CC),
           }
         })
         .object(dataCollection)
 
-        fs.writeFile(masterDataFileName, JSON.stringify(nestedData, undefined, 2), err => {
+      fs.writeFile(
+        masterDataFileName,
+        JSON.stringify(nestedData, undefined, 2),
+        (err) => {
           if (err) reject(err)
 
           console.log('Masterdata created')
           resole()
-        })
+        }
+      )
     })
   })
 }
